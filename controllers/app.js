5431abd2b89171a8e32ac6a7650db3ae207e9072
vijaysudarsanam98 <51353941+vijaysudarsanam98@users.noreply.github.com>
2020-03-22 11:12:58 +0530

    app.js_control

--- controllers/app.js
+++ controllers/app.js
@@ -18,45 +18,12 @@ app.use(bodyParser());
 let users = require('./controllers/users');
 app.use('/users', users);
 
-//initiate conversation_questions controller
-let conversation_questions = require('./controllers/conversation_questions');
-app.use('/conversation_questions', conversation_questions);
-
-//initiate neighbourhoods controller
-let neighbourhoods = require('./controllers/neighbourhoods');
-app.use('/neighbourhoods', neighbourhoods);
-
-//initiate characteristics controller
-let characteristics = require('./controllers/characteristics');
-app.use('/characteristics', characteristics);
-
-//initiate gathering controller
-let gatherings = require('./controllers/gatherings');
-app.use('/gatherings', gatherings);
-
-//initiate story teller controller
-let story_tellers = require('./controllers/story_tellers');
-app.use('/story_tellers', story_tellers);
 
 //initiate web hooks
 let webhooks = require('./controllers/webhooks');
 app.use('/webhooks', webhooks);
 
-//initiate checkins controller
-let checkins = require('./controllers/checkins');
-app.use('/checkins', checkins);
 
-//initiate Conversation Partners controller
-let conversationPartners = require('./controllers/conversation_partners');
-app.use('/conversation_partners', conversationPartners);
-
-//initiate QR Code controller
-let qrCode = require('./controllers/qr_code');
-app.use('/qr_code', qrCode);
-
-//initiate leads controller
-let leads = require('./controllers/leads');
-app.use('/leads', leads);
 
 app.listen(port, function () {
     console.log('Server started');
